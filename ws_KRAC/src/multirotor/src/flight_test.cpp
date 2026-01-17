be793ddfbb6d5aa3d1af493f97927691f178cfce
seunguniii <seungun@yonsei.ac.kr>
2026-01-18 03:03:19 +0900

    Refactor flight control to use mission modes
    Refactor flight control logic to use mission modes instead of flight modes, adding landing functionality and modifying waypoint handling.

--- ws_KRAC/src/multirotor/src/flight_test.cpp
+++ ws_KRAC/src/multirotor/src/flight_test.cpp
@@ -13,6 +13,7 @@
 #include "px4_msgs/msg/trajectory_setpoint.hpp"
 #include "px4_msgs/msg/vehicle_command.hpp"
 #include "px4_msgs/msg/vehicle_odometry.hpp"
+#include "px4_msgs/msg/vehicle_land_detected.hpp"
 
 using namespace std::chrono;
 using namespace std::chrono_literals;
@@ -21,9 +22,13 @@ using namespace px4_msgs::msg;
 class OffboardControl : public rclcpp::Node {
   public:
     OffboardControl() : Node("test") {
-      odom_sub_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>("/fmu/out/vehicle_odometry", rclcpp::SensorDataQoS(),
-                           [this](const px4_msgs::msg::VehicleOdometry::SharedPtr msg) {
+      odom_sub_ = this->create_subscription<VehicleOdometry>("/fmu/out/vehicle_odometry", rclcpp::SensorDataQoS(),
+                           [this](const VehicleOdometry::SharedPtr msg) {
                            curr_odom_ = *msg; has_odom_ = true;});
+
+      landed_sub_ = this->create_subscription<VehicleLandDetected>("/fmu/out/vehicle_land_detected", rclcpp::SensorDataQoS(),
+                           [this](const VehicleLandDetected::SharedPtr msg) {
+                           landed_ = msg->landed;});
 
       offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
       trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
@@ -35,16 +40,32 @@ class OffboardControl : public rclcpp::Node {
           return;
         }
 
-        if (!armed_ && flight_mode_ != LANDED) {
+        if(!armed_ && mission_mode_ != FINISHED) {
           this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
           this->arm();
         }
 
         publish_offboard_control_mode();
-        publish_trajectory_setpoint();
+
+        switch(mission_mode_) {
+          case FLIGHT:
+            publish_trajectory_setpoint();
+            break;
+
+          case LANDING:
+            land();
+            break;
+
+          default:
+          case FINISHED:
+            if(landed_ && armed_) disarm();
+            if(!armed_) return;
+            break;
+        }
 
         offboard_setpoint_counter_++;
       };
+
       timer_ = this->create_wall_timer(100ms, timer_callback);
     };
 
@@ -56,25 +77,25 @@ class OffboardControl : public rclcpp::Node {
     rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
     rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
 
-    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<VehicleOdometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<VehicleLandDetected>::SharedPtr  landed_sub_;
 
     px4_msgs::msg::VehicleOdometry curr_odom_;
 
-    enum FlightMode {
+    enum MissionMode {
         FLIGHT,
-        LANDED
+        LANDING,
+        FINISHED
     };
 
-    FlightMode flight_mode_ = FLIGHT;
+    MissionMode mission_mode_ = FLIGHT;
 
     std::vector<std::array<float,3>> waypoints_ = {
-      {0.0f, 0.0f, -25.0f},
-      {100.0f, 0.0f, -25.0f},
-      {200.0f, 100.0f, -25.0f},
-      {200.0f, -100.0f, -25.0f},
-      {100.0f, 0.0f, -25.0f},
-      {0.0f, 0.0f, -25.0f},
-      {0.0f, 0.0f, 0.0f}
+      {0.0f, 0.0f, -4.0f},
+      {3.0f, 0.0f, -4.0f},
+      {0.0f, 0.0f, -4.0f},
+      {0.0f, 3.0f, -4.0f},
+      {0.0f, 0.0f, -4.0f},
     };
 
     uint64_t offboard_setpoint_counter_ {0};
@@ -82,19 +103,20 @@ class OffboardControl : public rclcpp::Node {
 
     bool has_odom_ = false;
     bool armed_ = false;
+    bool landed_ = false;
 
     int hold_counter_ = 0;
     const int HOLD_THRESHOLD = 20;
 
     void arm();
     void disarm();
+    void land();
 
     void publish_offboard_control_mode();
     void publish_trajectory_setpoint();
     void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
 
     void publish_vehicle_command(int command, float value);
-    float k = 1;
 };
 
 void OffboardControl::arm() {
@@ -138,28 +160,32 @@ void OffboardControl::publish_trajectory_setpoint() {
   float dist_to_wp = to_wp.norm();
   Eigen::Vector3f direction_v(to_wp.x()/dist_to_wp, to_wp.y()/dist_to_wp, to_wp.z()/dist_to_wp);
 
-  if (flight_mode_ == FLIGHT && armed_) {
+  if (mission_mode_ == FLIGHT && armed_) {
     msg.position = {wp[0], wp[1], wp[2]};
 
-    if (dist_to_wp < 3.0f) {
+    if (dist_to_wp < 0.7f) {
       hold_counter_++;
       if (hold_counter_ > HOLD_THRESHOLD) {
         RCLCPP_INFO(this->get_logger(), "[MULTIROTOR] Heading to waypoint %ld", wp_idx_ + 1);
         hold_counter_ = 0;
         wp_idx_++;
         if(wp_idx_ >= waypoints_.size()) {
-          RCLCPP_INFO(this->get_logger(), "[LANDING] Sending land command");
-          publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_LAND);
-          flight_mode_ = LANDED;
-          disarm();
+          RCLCPP_INFO(this->get_logger(), "[MULTIROTOR] Flight mode : LANDING");
+          mission_mode_ = LANDING;
         }
       }
     }
   }
-  else if (flight_mode_ == LANDED) return;
+  else return;
 
   msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
   trajectory_setpoint_publisher_->publish(msg);
+}
+
+void OffboardControl::land() {
+  publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND);
+  RCLCPP_INFO(this->get_logger(), "[Landing] Starting landing sequence");
+  mission_mode_ = FINISHED;
 }
 
 void OffboardControl::publish_vehicle_command(uint16_t command, float param1, float param2) {
